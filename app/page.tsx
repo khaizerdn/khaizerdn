c35b5f6c8ff45d41b0294743e5ac456ff481d130
khaizerdn <khaizerdn@gmail.com>
2025-12-04 08:32:16 +0800

    Remove Other Projects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Projects from '@/components/Projects'
-import OtherProjects from '@/components/OtherProjects'
 import Certificates from '@/components/Certificates'
 import Contact from '@/components/Contact'
 
@@ -11,7 +10,6 @@ export default function Home() {
       <Hero />
       <Projects />
       <Certificates />
-      <OtherProjects />
       <About />
       <Contact />
     </>
