06e59743cd59f0ec4f4dcb715f8efa3464dfe3cf
khaizerdn <khaizerdn@gmail.com>
2025-12-07 22:09:01 +0800

    Fix casing issues for Tooltip component build

--- components/tooltip/Tooltip.tsx
+++ components/tooltip/Tooltip.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { motion, AnimatePresence } from 'framer-motion'
-import styles from './tooltip.module.css'
+import styles from './Tooltip.module.css'
 
 interface TooltipProps {
   children: React.ReactNode
